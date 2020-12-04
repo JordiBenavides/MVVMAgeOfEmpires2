77cc62385c4a5c51c2a8b6a9b0eaf802fd0d0eb4
Jordi Milla Catalán <jb.90@hotmail.es>
2020-12-04 12:58:31 +0100

    Create Model.

---
+++ MVVMAgeOfEmpires2/MVVMAgeOfEmpires2/Model/Civilizations.swift
@@ -0,0 +1,9 @@
+//
+//  Civilizations.swift
+//  MVVMAgeOfEmpires2
+//
+//  Created by Jordi Milla on 04/12/2020.
+//  Copyright © 2020 Jordi Milla Catalan. All rights reserved.
+//
+
+import Foundation
